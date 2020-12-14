633ad09ed000b293f73a00a2d568ab4c6edd4f05
j-asif <jalal.asif97@gmail.com>
2020-12-14 11:06:51 -0400

    Initial commit. Image generates what is otherwise the equivalent of what would look like if computers had a seizure. It does not yet calculate coin value. The only thing it measures is my fury over why this will not work. Keep trying...

---
+++ coin-classification.py
@@ -0,0 +1,33 @@
+# imports the relevant libraries; numpy for computations and openCV for machine perception
+import numpy as np
+import cv2
+
+# read the image and render it black and white (reducing colours makes the application easier); also, it starts to detec
+# circles everywhere for some reason
+img = cv2.imread('coins.jpeg', cv2.IMREAD_GRAYSCALE)
+# original image
+original_img = cv2.imread('coins.jpeg', 1)
+# adds some blur to reduce clarity; else it detects circles everywhere
+img = cv2.GaussianBlur(img, (5, 5), 0)
+
+
+
+circles = cv2.HoughCircles(img,cv2.HOUGH_GRADIENT,1,20,
+                            param1=20,param2=15,minRadius=30,maxRadius=60)
+
+# Rounds up the coordinates
+circles = np.uint16(np.around(circles))
+count = 0
+for i in circles[0,:]:
+    # draw the outer circle
+    cv2.circle(original_img,(i[0],i[1]),i[2],(0,255,0),2)
+    # draw the center of the circle
+    cv2.circle(original_img,(i[0],i[1]),2,(0,0,255),3)
+    #cv2.putText(original_img, str(count), (i[0],i[1]), cv2.FONT_HERSHEY_SCRIPT_SIMPLEX, 2, (0,0,0), 2)
+
+print(circles)
+
+# Commented out; these three lines of code are simply there to generate the window to show the code works
+cv2.imshow("Img", original_img)
+cv2.waitKey(1)
+cv2.destroyAllWindows()
