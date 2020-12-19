17d3c61376f54af2e0643908cc0ed0b68c8a6d74
j-asif <jalal.asif97@gmail.com>
2020-12-18 22:50:56 -0400

    Summation code for scraped dollar values obtained; fails to print them out on image. Correctness of sum untested, will get back to this later

--- coin-classification.py
+++ coin-classification.py
@@ -8,6 +8,13 @@ def compute_radii (coins):
          radii.append(rounded_thingy[2])
      return radii
 
+def av_pix (img, circle, size):
+    av_value = []
+    for rounded_thingy in circles[0,:]:
+        col = np.mean(img[rounded_thingy[1]-size:rounded_thingy[1]+size, rounded_thingy[0]-size:rounded_thingy[0]+size])
+        av_value.append(col)
+        return av_value
+
 # read the image and render it black and white (reducing colours makes the application easier); also, it starts to detec
 # circles everywhere for some reason
 img = cv2.imread('coins.jpeg', cv2.IMREAD_GRAYSCALE)
@@ -33,6 +40,22 @@ for i in circles[0, :]:
 # print(circles)
 radii = compute_radii(circles)
 print(radii)
+
+# print brightness values
+light_intensity = av_pix(img, circles, 20)
+print(light_intensity)
+
+values = []
+for a,b in zip(light_intensity, radii):
+    if (a>=75 & b >105):
+        values.append(2)
+    print(values)
+    count_2 = 0
+    for i in circles[0,:]:
+        cv2.putText(original_img, str(values[count_2]) + "$", (i[0],i[1]), cv2.FONT_HERSHEY_SCRIPT_SIMPLEX, 2, (0,0,0), 2 )
+        count_2 += 1
+    cv2.putText(original_img, "Estimated Total Value: " + "$" + str(sum(values)), (200,100), cv2.FONT_HERSHEY_SCRIPT_SIMPLEX, 1.3, 255 )
+
 
 # Commented out; these three lines of code are simply there to generate the window to show the code works
 cv2.imshow("Img", original_img)
