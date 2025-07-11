1f3a1ff06b5e1605d71c5e98cba27824219ff11e
Jalal Asif <jalalasif@Jalals-MacBook-Pro.local>
2025-07-11 06:20:31 -0300

    added calculation errors

--- coin-classification.py
+++ coin-classification.py
@@ -1,63 +1,68 @@
-# imports the relevant libraries; numpy for computations and openCV for machine perception
 import numpy as np
 import cv2
 
-def compute_radii (coins):
-     radii = []
-     for rounded_thingy in circles[0,:]: # Coudn't think of another name. No, really.
-         radii.append(rounded_thingy[2])
-     return radii
+def compute_radii(coins):
+    radii = []
+    for rounded_thingy in coins[0, :]:
+        radii.append(rounded_thingy[2])
+    return radii
 
-def av_pix (img, circle, size):
+def av_pix(img, circles, size):
     av_value = []
-    for rounded_thingy in circles[0,:]:
-        col = np.mean(img[rounded_thingy[1]-size:rounded_thingy[1]+size, rounded_thingy[0]-size:rounded_thingy[0]+size])
+    for rounded_thingy in circles[0, :]:
+        col = np.mean(
+            img[rounded_thingy[1]-size:rounded_thingy[1]+size,
+                rounded_thingy[0]-size:rounded_thingy[0]+size]
+        )
         av_value.append(col)
-        return av_value
+    return av_value
 
-# read the image and render it black and white (reducing colours makes the application easier); also, it starts to detec
-# circles everywhere for some reason
+# Load image (grayscale for processing, color for display)
 img = cv2.imread('coins.jpeg', cv2.IMREAD_GRAYSCALE)
-# original image
 original_img = cv2.imread('coins.jpeg', 1)
-# adds some blur to reduce clarity; else it detects circles everywhere
+
+# Blur to reduce noise
 img = cv2.GaussianBlur(img, (5, 5), 0)
 
+# Detect circles
 circles = cv2.HoughCircles(img, cv2.HOUGH_GRADIENT, 1, 460,
-                           param1=47, param2=49, minRadius=80, maxRadius= 120)
+                           param1=47, param2=49, minRadius=80, maxRadius=120)
 
-# Rounds up the coordinates
+# If no circles detected
+if circles is None:
+    print("No circles found.")
+    exit()
+
+# Round and draw circles
 circles = np.uint16(np.around(circles))
-count = 0
-for i in circles[0, :]:
-    # draw the outer circle
+for idx, i in enumerate(circles[0, :]):
     cv2.circle(original_img, (i[0], i[1]), i[2], (0, 255, 0), 2)
-    # draw the center of the circle
     cv2.circle(original_img, (i[0], i[1]), 2, (0, 0, 255), 3)
-    cv2.putText(original_img, str(count) , (i[0], i[1]), cv2.FONT_HERSHEY_SCRIPT_SIMPLEX, 2, (0, 0, 0), 2)
-    count += 1
+    cv2.putText(original_img, str(idx), (i[0], i[1]),
+                cv2.FONT_HERSHEY_SCRIPT_SIMPLEX, 2, (0, 0, 0), 2)
 
-# print(circles)
+# Measure radii and light intensity
 radii = compute_radii(circles)
-print(radii)
-
-# print brightness values
 light_intensity = av_pix(img, circles, 20)
-print(light_intensity)
 
+# Classify coin values
 values = []
-for a,b in zip(light_intensity, radii):
-    if (a>=75 & b >105):
+for a, b in zip(light_intensity, radii):
+    if a >= 75 and b > 105:
         values.append(2)
-    print(values)
-    count_2 = 0
-    for i in circles[0,:]:
-        cv2.putText(original_img, str(values[count_2]) + "$", (i[0],i[1]), cv2.FONT_HERSHEY_SCRIPT_SIMPLEX, 2, (0,0,0), 2 )
-        count_2 += 1
-    cv2.putText(original_img, "Estimated Total Value: " + "$" + str(sum(values)), (200,100), cv2.FONT_HERSHEY_SCRIPT_SIMPLEX, 1.3, 255 )
+    else:
+        values.append(1)
 
+# Annotate coin values
+for idx, i in enumerate(circles[0, :]):
+    cv2.putText(original_img, f"{values[idx]}$", (i[0], i[1]+50),
+                cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 0), 2)
 
-# Commented out; these three lines of code are simply there to generate the window to show the code works
-cv2.imshow("Img", original_img)
+# Annotate total
+cv2.putText(original_img, "Estimated Total Value: $" + str(sum(values)),
+            (100, 100), cv2.FONT_HERSHEY_SIMPLEX, 1.3, (0, 0, 0), 2)
+
+# Display image
+cv2.imshow("Detected Coins", original_img)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
